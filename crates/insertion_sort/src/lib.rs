10f10cf8509da69e5fd73fcc2222d3b886b1daf8
taizu-jin <mantas.scc@gmail.com>
2023-08-07 15:06:50 +0300

    test: test insertion sort

--- crates/insertion_sort/src/lib.rs
+++ crates/insertion_sort/src/lib.rs
@@ -8,3 +8,17 @@ pub fn sort<T: PartialOrd>(arr: &mut [T]) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::sort;
+
+    #[test]
+    fn test_insertion_sort() {
+        let mut arr = [5, 2, 4, 6, 1, 3];
+
+        sort(&mut arr);
+
+        assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
+    }
+}
